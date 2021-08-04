6286d3be5c95f16ec66bd694b8b6203c0a8690ea
Tom Louwerse <tomlouwerse@gmail.com>
2021-08-04 11:43:11 +0200

    Remove references to deprecated dplyr syntax

--- R/cosponsors.R
+++ R/cosponsors.R
@@ -16,10 +16,13 @@ cosponsors <- function(voteList, partylevel = FALSE) {
     # Create list with sponsors
     sponsors <- voteList$sponsorList %>%
       dplyr::mutate(isSponsor=1) %>%
-      dplyr::select_("c(id, sponsorName, sponsorId, sponsorParty, isSponsor)")
+      dplyr::select(dplyr::any_of(c("id", "sponsorName", "sponsorId", "sponsorParty", "isSponsor")))
   
     # Create matrix from this
-    sponsorMatrix <- tidyr::spread_(sponsors, "id", "isSponsor", fill=0)
+    sponsorMatrix <- tidyr::pivot_wider(sponsors, 
+                                        names_from="id", 
+                                        values_from="isSponsor", 
+                                        values_fill=0)
   
     # Matrix operation to calculate co-sponsorship
     ninfo <- ncol(sponsors) - 2
@@ -31,14 +34,12 @@ cosponsors <- function(voteList, partylevel = FALSE) {
       as.data.frame() %>%
       tibble::rownames_to_column(var="MP1number")
     
-    coSponsor <- tidyr::gather_(as.data.frame(coSponsor),
-                               "MP2number", "nCosponsor", colnames(coSponsor)[-1])
-  
+    coSponsor <- tidyr::pivot_longer(data=as.data.frame(coSponsor),
+                                     cols=!dplyr::contains("MP1number"),
+                                     names_to="MP2number", 
+                                     values_to="nCosponsor")
+   
     # Create dataframe combining numbers with names
-    # sponsorNames <- sponsorMatrix %>%
-    #   dplyr::select_(.dots=c("sponsorName", "sponsorId", "sponsorParty")) %>%
-    #   dplyr::mutate_(.dots=setNames(list("as.character(1:n())"),"MPnumber"))
-    
     sponsorNames <- sponsorMatrix %>%
       dplyr::select(c("sponsorName", "sponsorId", "sponsorParty")) %>%
       dplyr::mutate(MPnumber = as.character(1:dplyr::n()))
@@ -47,37 +48,37 @@ cosponsors <- function(voteList, partylevel = FALSE) {
     # Merge coSponsor with names for MP1
     coSponsor <- dplyr::left_join(coSponsor, sponsorNames,
                                   by=c("MP1number"="MPnumber")) %>%
-      dplyr::rename_(.dots=setNames("sponsorName", "MP1.name")) %>%
-      dplyr::rename_(.dots=setNames("sponsorId", "MP1.id")) %>%
-      dplyr::rename_(.dots=setNames("sponsorParty", "MP1.party"))
+      dplyr::rename("MP1.name"="sponsorName") %>%
+      dplyr::rename("MP1.id"="sponsorId") %>%
+      dplyr::rename("MP1.party"="sponsorParty")
   
     # Merge coSponsor with names for MP2
     coSponsor <- dplyr::left_join(coSponsor, sponsorNames,
                                   by=c("MP2number"="MPnumber")) %>%
-      dplyr::rename_(.dots=setNames("sponsorName", "MP2.name")) %>%
-      dplyr::rename_(.dots=setNames("sponsorId", "MP2.id")) %>%
-      dplyr::rename_(.dots=setNames("sponsorParty", "MP2.party"))
+      dplyr::rename("MP2.name"="sponsorName") %>%
+      dplyr::rename("MP2.id"="sponsorId") %>%
+      dplyr::rename("MP2.party"="sponsorParty")
   
   
     # Select only relevant variables and order
     coSponsor <- coSponsor %>%
-      dplyr::select_(.dots=list("MP1.id", "MP1.name", "MP1.party",
+      dplyr::select(dplyr::all_of(c("MP1.id", "MP1.name", "MP1.party",
                     "MP2.id", "MP2.name", "MP2.party",
-                    "nCosponsor"))
+                    "nCosponsor")))
   
     # Create dataframe with total number of proposals sponsored
     # (= number of proposals coSponsored with oneself)
     totalSponsor <- coSponsor %>%
-      dplyr::filter_(.dots="MP1.id==MP2.id & MP1.party==MP2.party") %>%
-      dplyr::select_(.dots=list("MP1.id", "MP1.name", "MP1.party",
-                                "nCosponsor")) %>%
-      dplyr::rename_(.dots=setNames("nCosponsor", "totalSponsor"))
+      dplyr::filter(.data$MP1.id== .data$MP2.id & .data$MP1.party== .data$MP2.party) %>%
+      dplyr::select(dplyr::all_of(c("MP1.id", "MP1.name", "MP1.party",
+                                "nCosponsor"))) %>%
+      dplyr::rename("totalSponsor"="nCosponsor")
   
     # Join coSponsor and totalSponsor and add percent Sponsorship match
     coSponsor <- dplyr::left_join(coSponsor, totalSponsor,
                                   by = c("MP1.id", "MP1.name", "MP1.party")) %>%
-      dplyr::mutate_(.dots=setNames("nCosponsor / totalSponsor", "percCosponsor")) %>%
-      dplyr::arrange_(.dots="MP1.name")
+      dplyr::mutate(percCosponsor = .data$nCosponsor / .data$totalSponsor) %>%
+      dplyr::arrange("MP1.name")
   
     return(coSponsor)
   } else {
@@ -85,11 +86,14 @@ cosponsors <- function(voteList, partylevel = FALSE) {
     # party cosponsor
     sponsors <- voteList$sponsorList %>%
       dplyr::mutate(isSponsor=1) %>%
-      dplyr::select_("c(id,sponsorParty,isSponsor)") %>%
+      dplyr::select(dplyr::all_of(c("id","sponsorParty","isSponsor"))) %>%
       dplyr::distinct()
     
     # Create matrix from this
-    sponsorMatrix <- tidyr::spread_(sponsors, "id", "isSponsor", fill=0)
+    sponsorMatrix <- tidyr::pivot_wider(sponsors, 
+                                        names_from="id", 
+                                        values_from="isSponsor", 
+                                        values_fill=0)
     
     # Matrix operation to calculate co-sponsorship
     ninfo <- ncol(sponsors) - 2
@@ -102,45 +106,47 @@ cosponsors <- function(voteList, partylevel = FALSE) {
       tibble::rownames_to_column(var="Party1number")
     
     #
-    coSponsor <- tidyr::gather_(as.data.frame(coSponsor),
-                                "Party2number", "nCosponsor", colnames(coSponsor)[-1])
+    coSponsor <- tidyr::pivot_longer(data=as.data.frame(coSponsor),
+                                     cols=!dplyr::contains("Party1number"),
+                                     names_to="Party2number", 
+                                     values_to="nCosponsor")
     
+
     # Create dataframe combining numbers with names
     sponsorNames <- sponsorMatrix %>%
-      dplyr::select_(.dots=c("sponsorParty")) %>%
-      dplyr::mutate_(.dots=setNames(list("as.character(1:n())"),"Partynumber"))
+      dplyr::select(c("sponsorParty")) %>%
+      dplyr::mutate(Partynumber = as.character(1:dplyr::n()))
     
     
     # Merge coSponsor with party Names
     coSponsor <- dplyr::left_join(coSponsor, sponsorNames,
                                   by=c("Party1number"="Partynumber")) %>%
-      dplyr::rename_(.dots=setNames("sponsorParty", "Party1"))
+      dplyr::rename("Party1"="sponsorParty")
     
-    # Merge coSponsor with names for MP2
+    # Merge coSponsor with party names for party 2
     coSponsor <- dplyr::left_join(coSponsor, sponsorNames,
                                   by=c("Party2number"="Partynumber")) %>%
-      dplyr::rename_(.dots=setNames("sponsorParty", "Party2"))
+      dplyr::rename("Party2"="sponsorParty")
     
     
     # Select only relevant variables and order
     coSponsor <- coSponsor %>%
-      dplyr::select_(.dots=list("Party1",
-                                "Party2",
-                                "nCosponsor"))
+      dplyr::select(dplyr::all_of(c("Party1", "Party2", "nCosponsor")))
+    
     
     # Create dataframe with total number of proposals sponsored
     # (= number of proposals coSponsored with oneself)
     totalSponsor <- coSponsor %>%
-      dplyr::filter_(.dots="Party1==Party2") %>%
-      dplyr::select_(.dots=list("Party1",
-                                "nCosponsor")) %>%
-      dplyr::rename_(.dots=setNames("nCosponsor", "totalSponsor"))
+      dplyr::filter(.data$Party1==.data$Party2) %>%
+      dplyr::select(dplyr::all_of(c("Party1","nCosponsor"))) %>%
+      dplyr::rename("totalSponsor"="nCosponsor")
+    
     
     # Join coSponsor and totalSponsor and add percent Sponsorship match
     coSponsor <- dplyr::left_join(coSponsor, totalSponsor,
                                   by = c("Party1")) %>%
-      dplyr::mutate_(.dots=setNames("nCosponsor / totalSponsor", "percCosponsor")) %>%
-      dplyr::arrange_(.dots="Party1")
+      dplyr::mutate(percCosponsor = .data$nCosponsor / .data$totalSponsor) %>%
+      dplyr::arrange("Party1")
     
     return(coSponsor)
       
