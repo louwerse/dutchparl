98ab8a3fd3a4c9f97dca36dd3a0b2239a8f25367
Tom Louwerse <tomlouwerse@gmail.com>
2021-08-04 13:32:05 +0200

    Add pkgdown documentation

--- R/download.R
+++ R/download.R
@@ -1,15 +1,36 @@
-#' Download Dutch Parliamentary Behaviour Dataset from the internet
+#' Download Dutch Parliamentary Behaviour Vote Dataset from the internet
 #'
-#' @return voteList object
+#' @return Nothing. Side effect is to download the file.
 #' @param url URL to download from. Defaults to downloading from dutchparl dataverse.
-#' @param destfile Destination file.
+#' @param destfile Destination file (rds).
 #' @export
 #' @examples
 #' \dontrun{
 #' downloadVotes()
 #' }
-downloadVotes <- function(url=NULL,destfile="votes.RData") {
-  cat("Creative Commons Attribution 4.0 International license applies. See: https://creativecommons.org/licenses/by/4.0/legalcode.")
-  if(is.null(url)) url <- "https://dataverse.harvard.edu/api/access/datafile/3000707?gbrecs=true"
-  utils::download.file(url, destfile=destfile)
+downloadVotes <- function(url=NULL,destfile="votes.rds") {
+  cat("Creative Commons Attribution 4.0 International license applies. See: https://creativecommons.org/licenses/by/4.0/legalcode.\r\n")
+  if(is.null(url)) url <- "https://dataverse.harvard.edu/api/access/datafile/4279819?gbrecs=true"
+  utils::download.file(url, destfile=destfile, mode="wb")
 }
+
+#' Download Dutch Parliamentary Behaviour Questions Dataset from the internet
+#'
+#' @return Nothing. Side effect is to download the file.
+#' @param url URL to download from. Defaults to downloading from dutchparl dataverse.
+#' @param destfile Destination file (rds).
+#' @export
+#' @examples
+#' \dontrun{
+#' downloadQuestions()
+#' }
+downloadQuestions <- function(url=NULL,destfile="questions.rds") {
+  cat("Creative Commons Attribution 4.0 International license applies. See: https://creativecommons.org/licenses/by/4.0/legalcode.\r\n")
+  if(is.null(url)) url <- "https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/5EEXZY/CQFKD9&version=1.0"
+  utils::download.file(url, destfile=destfile, mode="wb")
+}
+
+
+
+
+
