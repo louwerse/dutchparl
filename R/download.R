1ba3361720205b918ed0672a954daa64c17e558b
Tom Louwerse <t.p.louwerse@fsw.leidenuniv.nl>
2026-04-10 23:13:48 +0200

    Allow setting custom download location

--- R/download.R
+++ R/download.R
@@ -2,12 +2,13 @@
 #'
 #' @return voteList object
 #' @param url URL to download from. Defaults to downloading from dutchparl dataverse.
+#' @param destfile Destination file.
 #' @export
 #' @examples
 #' \dontrun{
 #' downloadVotes()
 #' }
-downloadVotes <- function(url=NULL) {
+downloadVotes <- function(url=NULL,destfile="votes.RData") {
   if(is.null(url)) url <- "https://dl.dropboxusercontent.com/u/31727287/Other/2016/votes.RData"
-  utils::download.file(url, destfile="votes.RData")
+  utils::download.file(url, destfile=destfile)
 }
